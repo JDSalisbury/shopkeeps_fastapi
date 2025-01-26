d19306e0be1a501b60232b0aca39df7037b07810
Jeff <salisbury.jeffery@gmail.com>
2025-01-26 18:18:27 -0500

    DB setup with new render db.

--- models/db_setup.py
+++ models/db_setup.py
@@ -1,11 +1,14 @@
 from sqlmodel import Session, SQLModel, create_engine
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+render_external_url = os.getenv("DATABASE_URL")
 
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(
+    render_external_url, pool_pre_ping=True
+)
 
 
 def create_db_and_tables():
